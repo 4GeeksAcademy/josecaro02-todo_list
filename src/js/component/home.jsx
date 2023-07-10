72da4b08894c59281737ffe9434ee6612ef739d8
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2023-07-10 17:32:42 +0000

    Add explanation class Todo list class

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [name, setName] = useState("Jose");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center mt-5">
+			<label >Nombre</label>
+			<input type="text" placeholder="Ingresa tu nombre" value={name} onChange={(e)=>{
+				setName(e.target.value)
+			}}/>
+			<button className="btn btn-success" onClick={()=>{
+				setName("")
+			}}>Limpiar input</button>
+			<button className="btn btn-warning" onClick={()=>{
+				name == "" ? alert("Ingresa tu nombre") : ""
+			}}>Check input</button>
+			<h3>Tu nombre es {name}</h3>
 		</div>
 	);
 };
